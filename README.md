decb9418c18ff5a4a7eaa2e0d2dc5f6e4437a699
Jacob Aronoff <jacobaronoff45@gmail.com>
2017-12-31 12:46:29 -0500

    Added categories and more docs

--- README.md
+++ README.md
@@ -18,21 +18,100 @@ What action would you like to do?
 ## Commands
 
 * Help
-	* Get the avaialble commands
+	* Get the available commands
 * Next n items
 	* Display the next {n} items you need to watch
 * Watched n items
 	* Mark that you watched {n} items
 * Unwatched n items
-	* Unmark that you watched {n} items, maybe you want to rewatch some more, or you messed up marking
+	* Unmark that you watched {n} items,maybe you want to rewatch some more, or you messed up marking
 * Get info
 	* Get some basic metadata
+* Get categories
+	* Get avaialble categories for filtering
+* Set categories (comma seperated categories)
+	* Set avaialble categories to filter
+	* Example: Movie,Tv,Other
 * Exit
 	* Self explanatory
 
+## Examples
+
+```
+What action would you like to do?
+> next 10 items
+==========NEXT 10 ITEMS==========
+1:	Captain America: The First Avenger
+2:	Agent Carter S01E01: Now is Not the End
+3:	Agent Carter S01E02: Bridge and Tunnel
+4:	Agent Carter S01E03: Time and Tide
+5:	Agent Carter S01E04: The Blitzkrieg Button
+6:	Agent Carter S01E05: The Iron Ceiling
+7:	Agent Carter S01E06: A Sin to Err
+8:	Agent Carter S01E07: Snafu
+9:	Agent Carter S01E08: Valediction
+10:	Agent Carter
+==========NEXT 10 ITEMS==========
+What action would you like to do?
+> watch 2 items
+What action would you like to do?
+> next 10 items
+==========NEXT 10 ITEMS==========
+1:	Agent Carter S01E02: Bridge and Tunnel
+2:	Agent Carter S01E03: Time and Tide
+3:	Agent Carter S01E04: The Blitzkrieg Button
+4:	Agent Carter S01E05: The Iron Ceiling
+5:	Agent Carter S01E06: A Sin to Err
+6:	Agent Carter S01E07: Snafu
+7:	Agent Carter S01E08: Valediction
+8:	Agent Carter
+9:	Agent Carter S02E01: The Lady in the Lake
+10:	Agent Carter S02E02: A View in the Dark
+==========NEXT 10 ITEMS==========
+What action would you like to do?
+> unwatch 1 item
+What action would you like to do?
+> next 5 items
+==========NEXT 5 ITEMS==========
+1:	Agent Carter S01E01: Now is Not the End
+2:	Agent Carter S01E02: Bridge and Tunnel
+3:	Agent Carter S01E03: Time and Tide
+4:	Agent Carter S01E04: The Blitzkrieg Button
+5:	Agent Carter S01E05: The Iron Ceiling
+==========NEXT 5 ITEMS==========
+What action would you like to do?
+> get info
+==========GET INFO==========
+	1 pieces of content
+	124 hours of content
+	250 pieces of content left
+	12017 hours of content left
+==========GET INFO==========
+What action would you like to do?
+> set categories movie
+What action would you like to do?
+> get categories
+==========GET Categories==========
+Current Categories:
+movie
+Available Categories:
+tv,movie,other
+==========GET Categories==========
+What action would you like to do?
+> next 5 items
+==========NEXT 5 ITEMS==========
+1:	Iron Man
+2:	Iron Man 2
+3:	The Incredible Hulk
+4:	Thor
+5:	Avengers
+==========NEXT 5 ITEMS==========
+```
+
+
 ## Next Steps
 
-I'm going to be adding in some more metadata and info for different items in the rewatch. I'm planning on connecting iMDB and a couple of other APIs that would get information on where to watch it.
+I'm going to be adding in some more metadata and info for different items in the rewatch. I'm planning on connecting iMDB and a couple of other APIs that would get information on where to watch it. I'm also going to add in information like if you're only binging on TV, Movies, or everything.
 
 
 
--- tracker.py
+++ tracker.py
@@ -32,6 +32,10 @@ class MarvelTracker(object):
             {
                 "regex": re.compile('(get info)'),
                 "function": self.get_info
+            },
+            {
+                "regex": re.compile('(get categories)'),
+                "function": self.get_categories
             }
         ]
         if not os.path.isfile("./" + self.copyName):
@@ -42,6 +46,16 @@ class MarvelTracker(object):
             file = open(self.copyName, 'rb')
             read_copy = csv.reader(file)
             self.trackedFile = list(read_copy)
+        self.available_categories = set(
+            [t[2].lower() for t in self.trackedFile[1:] if t[2] is not ''])
+        self.current_categories = self.available_categories
+        temp = '(' + '|'.join(map(lambda x: x,
+                                  self.available_categories)) + ')'
+        set_reg = '(set categories)( +)(' + temp + ',)*' + temp + '$'
+        self.regexes.append({
+            "regex": re.compile(set_reg),
+            "function": self.set_categories
+        })
 
     def __exit__(self, exc_type, exc_value, traceback):
         with open(self.copyName, 'wb') as f:
@@ -52,14 +66,14 @@ class MarvelTracker(object):
         return self
 
     def make_copy(self, orig):
-        all = []
+        allData = []
         row = next(orig)
         row.append("watched")
-        all.append(row)
+        allData.append(row)
         for row in orig:
             row.append(False)
-            all.append(row)
-        self.trackedFile = all
+            allData.append(row)
+        self.trackedFile = allData
 
     def add_csv(self, csvName):
         return csvName + ".csv"
@@ -69,12 +83,16 @@ class MarvelTracker(object):
             return v
         return v.lower() in ("yes", "true", "t", "1")
 
+    def matchesCategories(self, i):
+        return self.trackedFile[i][2].lower() in self.current_categories
+
     def get_next_n_items(self, n):
         cur = 0
         i = 1
         print("==========NEXT {0} ITEMS==========".format(n))
         while cur < n and self.guard_bounds(i):
-            if not self.str2bool(self.trackedFile[i][2]):
+            if not self.str2bool(self.trackedFile[i][3]) and \
+                    self.matchesCategories(i):
                 cur += 1
                 print("{0}:\t{1}".format(cur, self.trackedFile[i][0]))
             i += 1
@@ -84,31 +102,46 @@ class MarvelTracker(object):
         cur = 0
         i = 1
         while self.guard_bounds(i):
-            if not self.str2bool(self.trackedFile[i][2]) and cur < n:
-                self.trackedFile[i][2] = True
+            if not self.str2bool(self.trackedFile[i][3]) and cur < n and \
+                    self.matchesCategories(i):
+                self.trackedFile[i][3] = True
                 cur += 1
             i += 1
 
     def unwatch_n_items(self, n):
         cur = 0
-        i = 1
-        while i < len(self.trackedFile) - 2 and self.guard_bounds(i):
-            if self.str2bool(self.trackedFile[i][2]) and \
-                    not self.str2bool(self.trackedFile[i + 1][2]):
+        i = len(self.trackedFile) - 2
+        while i > 1 and self.guard_bounds(i):
+            if self.str2bool(self.trackedFile[i][3]) and \
+                    not self.str2bool(self.trackedFile[i + 1][3]) and \
+                    self.matchesCategories(i):
                 break
-            i += 1
-        while cur < n and self.guard_bounds(i):
-            self.trackedFile[i][2] = False
+            i -= 1
+        while cur < n and self.guard_bounds(i) and \
+                self.matchesCategories(i):
+            self.trackedFile[i][3] = False
             i -= 1
             cur += 1
+
+    def set_categories(self, user_input):
+        self.current_categories = [
+            t for t in self.available_categories if t in user_input]
 
     def guard_bounds(self, n):
         return n < len(self.trackedFile) and n >= 0
 
+    def get_categories(self, _):
+        print("==========GET Categories==========")
+        print("Current Categories:")
+        print(','.join(self.current_categories))
+        print("Available Categories:")
+        print(','.join(self.available_categories))
+        print("==========GET Categories==========")
+
     def get_info(self, _):
         print("==========GET INFO==========")
-        watched = [t for t in self.trackedFile if self.str2bool(t[2])]
-        unwatched = [t for t in self.trackedFile if not self.str2bool(t[2])]
+        watched = [t for t in self.trackedFile if self.str2bool(t[3])]
+        unwatched = [t for t in self.trackedFile if not self.str2bool(t[3])]
         hours = sum([int(t[1]) for t in watched if t[1].isdigit()])
         hoursTotal = sum([int(t[1]) for t in unwatched if t[1].isdigit()])
         print("\t{0} pieces of content".format(len(watched)))
@@ -123,7 +156,7 @@ class MarvelTracker(object):
         print("==========HELP==========")
         print("Available commands")
         print("\t* Help")
-        print("\t\t* Get the avaialble commands")
+        print("\t\t* Get the available commands")
         print("\t* Next n items")
         print("\t\t* Display the next {n} items you need to watch")
         print("\t* Watched n items")
@@ -133,6 +166,11 @@ class MarvelTracker(object):
               "maybe you want to rewatch some more, or you messed up marking")
         print("\t* Get info")
         print("\t\t* Get some basic metadata")
+        print("\t* Get categories")
+        print("\t\t* Get avaialble categories for filtering")
+        print("\t* Set categories (comma seperated categories)")
+        print("\t\t* Set avaialble categories to filter")
+        print("\t\t* Example: Movie,Tv,Other")
         print("\t* Exit")
         print("\t\t* Self explanatory")
 
@@ -140,7 +178,7 @@ class MarvelTracker(object):
         nums = [int(s) for s in user_input.split() if s.isdigit()]
         for r in self.regexes:
             if r["regex"].match(user_input):
-                r["function"](nums[0] if len(nums) > 0 else 0)
+                r["function"](nums[0] if len(nums) > 0 else user_input)
 
 
 if __name__ == '__main__':
